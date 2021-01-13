17c5e2e4f619acd5c6352f8e3e33c3e51068b514
Uchiman <utsman.khabillah@gmail.com>
2021-01-13 10:42:47 +0700

    Book Controller

--- app/Http/Controllers/Admin/DataController.php
+++ app/Http/Controllers/Admin/DataController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Author;
+use App\Book;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -16,6 +17,23 @@ class DataController extends Controller
                     ->addColumn('action', 'admin.author.action')
                     ->addIndexColumn()
                     ->rawColumns(['action'])
+                    ->toJson();
+    }
+
+    public function books()
+    {
+        $books = Book::orderBy('title', 'ASC');
+
+        return datatables()->of($books)
+                    ->addColumn('author', function(Book $model) {
+                        return $model->author->name;
+                    })
+                    ->editColumn('cover', function(Book $model) {
+                        return '<img src="'. $model->getCover() .'" height=150px >';
+                    })
+                    ->addColumn('action', 'admin.author.action')
+                    ->addIndexColumn()
+                    ->rawColumns(['cover', 'action'])
                     ->toJson();
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -11,4 +11,7 @@ Route::get('/', 'HomeController@index')->name('dashboard');
 
 
 Route::get('/author/data', 'DataController@authors')->name('author.data');
+Route::get('/book/data', 'DataController@books')->name('book.data');
+
 Route::resource('author', 'AuthorController');
+Route::resource('book', 'BookController');
